bbaaaf7ae12086d57b24e41160eb241713b8ea92
taiki1frsy <taiki.furusyo@gmail.com>
2022-12-20 11:47:09 +0900

    fix typo

--- yf-test-on-osmosis/src/msg.rs
+++ yf-test-on-osmosis/src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Coin, Decimal, Uint128, Uint64, Addr};
+use cosmwasm_std::Coin;
 // use osmosis_std::types::osmosis::gamm::v1beta1::SwapAmountInRoute;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -6,12 +6,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg { }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum Slipage {
-    MaxSlipagePercentage(Decimal),
-    MinOutputAmount(Uint128),
-}
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// pub enum Slipage {
+//     MaxSlipagePercentage(Decimal),
+//     MinOutputAmount(Uint128),
+// }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -22,7 +22,6 @@ pub enum ExecuteMsg {
         pool_id: u64,
         token_in: Coin,
         share_out_min_amount: String,
-        // slipage: Slipage,
     },
     // let the contract send a MsgExitSwapExternAmountOut to exit a single
     // token from a pool with one msg
@@ -33,11 +32,6 @@ pub enum ExecuteMsg {
         token_out_min_amount: String,
     }
 
-    // Deposit {
-    //     pool: Uint64,
-    //     share_min_out: Uint128,
-    //     // duration: Uint64,
-    // },
     // Lockup {
     //     timeout: Option<u64>,
     //     duration: Uint64,
@@ -47,10 +41,12 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // TestTwap {},
+    DepositerShareAmount {
+        depositor: String,
+    },
 }
 
-//  #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-// pub struct TestTwapResponse {
-//     pub price: String,
-// }
+ #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct DepositerShareAmount {
+    pub share_amount: String,
+}
