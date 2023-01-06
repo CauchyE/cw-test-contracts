d352abb155df58a60fbc335b64adf219b9569d26
taiki1frsh <taiki.furusho@gmail.com>
2023-01-06 19:58:04 +0900

    store and instantiate msg in cli

--- yf-test-on-osmosis/ci-scripts/msg.sh
+++ yf-test-on-osmosis/ci-scripts/msg.sh
@@ -6,13 +6,13 @@ CONTRACT_NAME="yf_test_on_osmosis-aarch64"
 NODE=http://localhost:26653
 CHAIN_ID=osmo-testing
 OSMO_HOME=$HOME/.osmosisd/validator1
+TXFLAG="--gas=auto --gas-prices 0.1uosmo --gas-adjustment 1.3 -b block --keyring-backend=test --home=$OSMO_HOME --node $NODE --chain-id $CHAIN_ID"
 
 # store bytecode and acquire code id of it
-RES=$(osmosisd tx wasm store artifacts/$CONTRACT_NAME.wasm --from validator1 \
-    --keyring-backend=test --home=$OSMO_HOME --node $NODE --chain-id $CHAIN_ID \
-    -y --output json -b block --gas=auto)
-echo $RES
-CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
+RES=$(osmosisd tx wasm store "./artifacts/yf_test_on_osmosis-aarch64.wasm" --from validator1 \
+    -y --output json $TXFLAG)
+# echo $RES
+CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[1].value')
 echo $CODE_ID
 
 # instantiate contract out of the uploaded code using that code id
@@ -20,5 +20,4 @@ echo $CODE_ID
 INIT='{}'
 
 osmosisd tx wasm instantiate $CODE_ID "$INIT" --from validator1 \
-    --keyring-backend=test --home=$OSMO_HOME --node $NODE \
-    --chain-id $CHAIN_ID -y --no-admin
+    $TXFLAG -y --no-admin --label yf_test_on_osmosis
